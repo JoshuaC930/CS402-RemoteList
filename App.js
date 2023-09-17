03a579c5def0a33787ef3ffe1c306f8eac11bcb5
JoshuaC930 <joshuacorrales930@u.boisestate.edu>
2023-09-17 15:29:44 -0600

    inital commit

---
+++ App.js
@@ -0,0 +1,238 @@
+import { TouchableOpacity, FlatList, StyleSheet, Text, View, Modal, TextInput, Button, Platform, StatusBar } from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView } from "react-native";
+import { ScrollView } from "react-native";
+
+//list varable for names
+var nameList = [
+    { key: 'Angel', selected: false },
+    { key: 'Azure', selected: false },
+    { key: 'Barret', selected: false },
+    { key: 'Cloud', selected: false },
+    { key: 'Tifa', selected: false },
+    { key: 'Aerith', selected: false }
+]
+
+//list object
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+    <TouchableOpacity style={[styles.item, backgroundColor]} onPress={onPress}>
+        <Text style={[styles.itemText, textColor]}>{item.key}</Text>
+    </TouchableOpacity>
+);
+
+//modal obeject for add
+const ModalInput = ({ onTextChange, onSubmit, visible, value, toggle }) => {
+    return (
+        <Modal
+            visible={visible}
+            transparent={true}
+            style={{ justifyContent: 'center' }}>
+            <View style={{ height: 100, marginTop: 70, padding: 20, minHeight: 150, width: '80%', alignSelf: 'center', justifyContent: 'center', backgroundColor: '#b5b5b5', borderRadius: 20 }}>
+                <Text style={{ alignSelf: "center", marginTop: 5, fontSize: 20 }}>Enter new item to add:</Text>
+                <TextInput
+                    style={{ fontSize: 20, padding: 5, marginTop: 10, marginBottom: 10, backgroundColor: 'white', borderRadius: 5 }}
+                    value={value}
+                    onChangeText={onTextChange}
+                    placeholder={'Value'}
+                />
+                <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                    <Button title='Add' style={styles.button} textStyle={styles.buttonText} onPress={onSubmit}>Add</Button>
+                    <Button title='Cancel' style={styles.button} textStyle={styles.buttonText} onPress={toggle}>Cancel</Button>
+                </View>
+            </View>
+        </Modal>
+    );
+}
+
+//main function
+const FlatListBasics = () => {
+    const [list, setList] = useState(nameList);
+    const [visible, setVisible] = useState(false);
+    const [text, onTextChange] = useState('');
+
+    const renderItem = ({ item }) => {
+        var backgroundColor, color;
+
+        backgroundColor = item.selected ? '#349eeb' : 'white';
+        color = item.selected ? 'white' : 'black';
+
+        return (<Item
+            item={item}
+            onPress={() => { toggleList(item.key) }}
+            backgroundColor={{ backgroundColor }}
+            textColor={{ color }}
+        />)
+
+        // called when a list item is touched
+        function toggleList(key) {
+            const newList = list.map((item) => {
+                if (item.key == key) item.selected = item.selected ? false : true;
+                return item;
+            });
+
+            setList([...newList]);
+        }
+
+    }
+
+    //function for adding name to list
+    const add = (value) => {
+        if (value == "") {
+            alert("Please enter a value.");
+        }
+        else {
+            const newList = list.concat({ key: value, selected: false });
+            setList([...newList]);
+        }
+    }
+
+    //function for removing item from list
+    function remove() {
+        const deletedList = list.filter((item) => item.selected == true);
+        const newList = list.filter((item) => item.selected != true);
+
+        if (deletedList.length == 0) {
+            alert("No item was selected. No items were removed from list.");
+        }
+        else {
+            var deletedItemsString = "";
+            for (var i = 0; i < deletedList.length; i++) {
+                deletedItemsString += "- " + deletedList[i].key + "\n"
+            }
+            alert("Deleted item(s): \n" + deletedItemsString);
+        }
+
+        setList([...newList]);
+    }
+
+    //function for joining entries
+    function join() {
+        var joinedString = "";
+        const newList = list.filter((item) => item.selected != true);
+        const joinedList = list.filter((item) => item.selected == true);
+
+        if (joinedList.length == 0) {
+            alert("No items were selected!");
+            return;
+        }
+
+        joinedList.forEach((item) => joinedString += item.key + ",");
+        const newJoinedList = newList.concat({ key: joinedString.substring(0, joinedString.length - 1), selected: false });
+
+        setList([...newJoinedList]);
+    }
+
+    function split() {
+        const selectedList = list.filter((item) => item.selected == true);
+
+        if (selectedList.length == 0) {
+            alert("No items were selected!");
+            return;
+        }
+
+        var newList = list;
+
+        selectedList.forEach((selectedItem) => {
+            if (selectedItem.key.includes(",") && !selectedItem.key.startsWith(",") && !selectedItem.key.endsWith(",")) {
+                newList = newList.filter((item) => { return item != selectedItem })
+                var splitArr = selectedItem.key.split(",")
+                splitArr.forEach((value) => newList = newList.concat({ key: value, selected: false }))
+                setList(newList)
+            }
+            else {
+                const uncheckedList = list.map((item) => {
+                    if (item.selected && selectedItem.key == item.key) item.selected = false
+                    return item;
+                })
+
+                setList([...uncheckedList])
+            }
+        })
+    }
+
+    //function for resetting list
+    function reset() {
+        setList([...nameList]);
+    }
+
+    //function for selecting all items on list
+    function selectAll() {
+        list.forEach((item) =>
+            toggleList(item)
+        )
+    }
+
+    return (
+        <SafeAreaView style={[styles.container, { paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 20 }]}>
+            <View>
+                <Text style={styles.title}>A Simple List</Text>
+                <Text style={styles.instruction}>Use the the buttons below to modify the list.</Text>
+            </View>
+            <View>
+                <View style={styles.buttonLayout}>
+                    <Button style={styles.button} title='Add' onPress={() => setVisible(!visible)} />
+                    <Button style={styles.button} title='Remove' onPress={() => remove()} />
+                    <Button title='Join' onPress={() => join()} />
+                    <Button title='Split' onPress={() => split()} />
+                    <Button title='Reset' onPress={() => reset()} />
+                </View>
+            </View>
+            <ModalInput
+                visible={visible}
+                value={text}
+                onTextChange={onTextChange}
+                toggle={() => setVisible(!visible)}
+                onSubmit={() => { onTextChange(''); add(text); setVisible(!visible) }}
+            />
+            <ScrollView style={styles.list}>
+                <FlatList data={list} renderItem={renderItem} />
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    title: {
+        fontSize: 48,
+        fontWeight: 'bold',
+        paddingLeft: 20
+    },
+    instruction: {
+        fontSize: 20,
+        paddingLeft: 20,
+        paddingTop: 3,
+        paddingBottom: 5
+    },
+    buttonLayout: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        margin: 20,
+    },
+    list: {
+        marginTop: 20,
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 10,
+        borderWidth: 2,
+        borderColor: 'gray',
+    },
+    item: {
+        paddingLeft: 20,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderBottomWidth: 1,
+        borderColor: 'gray',
+        overflow: 'hidden'
+    },
+    itemText: {
+        fontSize: 24
+    }
+})
+
+export default FlatListBasics;
---
+++ README.md
@@ -0,0 +1,31 @@
+# Project 1 - Simple List
+
+Written By: Joshua Corrales
+Class: CS402-001
+
+## Overview
+
+This is a simple list program designed to allow the user to manage a simple list. There is already data on it and the user
+can modify the contents of the list. The user is able to add, remove, join, and split list entries.
+
+## Usage
+
+You first need to use ```npm install``` to ensure you have all the required dependencies and ensure that you have the expo-cli by running
+```npm install -g expo-cli```. Once installed, you can open the project through a terminal window by navigating to the project directory and running ```expo start```, from there you can use the provided QR code and the ExpoGo app to run the application on your local device.
+
+
+## Reflection
+
+This was a fun project, I have some minor JavaScript experience so something did feel
+familar but overall this is new to me and it was fun to play around with. This is my first
+time with mobile development so making mobile experiencecs was a bit different. Some things
+reminded me of HTML like the tags but others were different and took a little but to get used to.
+For example, getting things to render on the screen to a bit of trial and error for me. Also initialing
+default functions was and still is confusing to me.
+
+## Sources used
+https://stackoverflow.com/questions/56645450/how-can-i-get-the-selected-item-from-list-in-react
+
+https://stackoverflow.com/questions/36326612/how-to-delete-an-item-from-state-array
+
+
